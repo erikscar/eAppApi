d160de1d2df08f23a630db0375662e6315935795
erikscar <erikscarcela@gmail.com>
2025-06-23 13:34:01 -0300

    feat: add admin default user in data mapping

--- Data/Mappings/UserMap.cs
+++ Data/Mappings/UserMap.cs
@@ -9,31 +9,37 @@ public class UserMap : BaseMap<User>
     public override void Configure(EntityTypeBuilder<User> builder)
     {
         base.Configure(builder);
-        
-        builder.Property(user => user.FirstName)
-        .HasColumnType("VARCHAR(100)");
 
-        builder.Property(user => user.LastName)
-        .HasColumnType("VARCHAR(100)");
+        builder.Property(user => user.FirstName).HasColumnType("VARCHAR(100)");
 
-        builder.Property(user => user.Email)
-        .IsRequired()
-        .HasColumnType("VARCHAR(100)");
+        builder.Property(user => user.LastName).HasColumnType("VARCHAR(100)");
 
-         builder.Property(user => user.Phone)
-        .HasColumnType("VARCHAR(50)");
+        builder.Property(user => user.Email).IsRequired().HasColumnType("VARCHAR(100)");
 
-        builder.Property(user => user.ImageUrl)
-        .HasColumnType("VARCHAR(255)");
+        builder.Property(user => user.Phone).HasColumnType("VARCHAR(50)");
 
-        builder.Property(user => user.PasswordHash)
-        .IsRequired()
-        .HasColumnType("VARCHAR(255)");
+        builder.Property(user => user.ImageUrl).HasColumnType("VARCHAR(255)");
 
-        builder.HasOne(user => user.Cart)
-        .WithOne(cart => cart.User)
-        .HasForeignKey<Cart>(cart => cart.UserId)
-        .IsRequired();
+        builder.Property(user => user.PasswordHash).IsRequired().HasColumnType("VARCHAR(255)");
 
+        builder
+            .HasOne(user => user.Cart)
+            .WithOne(cart => cart.User)
+            .HasForeignKey<Cart>(cart => cart.UserId)
+            .IsRequired();
+
+        builder.HasData(
+            new User
+            {
+                Id = 1,
+                FirstName = "Master",
+                LastName = "Admin",
+                Role = "Admin",
+                Phone = "(00) 00000-0000",
+                ImageUrl = "https://cdn-icons-png.flaticon.com/512/17003/17003310.png",
+                Email = "admin@gmail.com",
+                PasswordHash = "123",
+            }
+        );
     }
 }
---
+++ Migrations/20250623162623_AddDefaultAdminUser.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace eApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDefaultAdminUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "Email", "FirstName", "ImageUrl", "LastName", "PasswordHash", "Phone", "Role" },
+                values: new object[] { 1, "admin@gmail.com", "Master", "https://cdn-icons-png.flaticon.com/512/17003/17003310.png", "Admin", "123", "(00) 00000-0000", "Admin" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1);
+        }
+    }
+}
